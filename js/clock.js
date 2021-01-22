7fdde17de0c2d475dd620772b0a1aa3d30efd320
Gi-ChanKim <71857561+Gi-ChanKim@users.noreply.github.com>
2021-01-22 17:57:49 +0900

    210122 Create TimeToDo

---
+++ js/clock.js
@@ -0,0 +1,21 @@
+const clock = document.querySelector(".js-clock .clock__text");
+
+function getTime() {
+  const date = new Date();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+  const time = `${hours < 10 ? `0${hours}` : hours}:${
+    minutes < 10 ? `0${minutes}` : minutes
+  }:${seconds < 10 ? `0${seconds}` : seconds}`;
+
+  clock.innerHTML = time;
+  return;
+}
+function init() {
+  getTime();
+  setInterval(getTime, 1000);
+  return;
+}
+
+init();
